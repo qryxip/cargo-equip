039363a47a448ff562fa95f42601f74288c7ca86
Ryo Yamashita <qryxip@gmail.com>
2020-09-02 16:31:46 +0900

    Add `extern crate proc_macro` for compatibility

---
+++ cargo_equip_marker/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [Unreleased]
+
+### Fixed
+
+- Now this crate can be compiled with Rust 1.41.0.
--- cargo_equip_marker/src/lib.rs
+++ cargo_equip_marker/src/lib.rs
@@ -1,5 +1,11 @@
 //! Provides a marker attribute for `cargo-equip`.
 
+#![forbid(unsafe_code)]
+#![warn(rust_2018_idioms)]
+
+#[allow(unused_extern_crates)]
+extern crate proc_macro; // for compatibility
+
 /// Returns `item` as is.
 #[proc_macro_attribute]
 pub fn equip(_: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
